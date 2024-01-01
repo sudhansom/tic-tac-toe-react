687c3108bc3861047f914b577b55ddf70f4caf96
bhuwan <bkspoudel@gmail.com>
2024-01-01 18:13:09 +0100

    fix basic setup for tic-tac-toe

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  
+
+  return (
+    <div className="top-header">
+      <img src="/public/game-logo.png" height="100px" alt="" />
+      <h1>Tic-Tac-Toe</h1>
+    </div>
+  )
+}
+
+export default App
