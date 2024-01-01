087a2707e07809f2689bcc7de0e1a743abb50884
bhuwan <bkspoudel@gmail.com>
2024-01-01 18:59:40 +0100

    handle click on name too

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react'
 import "./Player.css"
 
 const Player = ({initialName}) => {
-    console.log(initialName);
     const [name, setName] = useState(initialName);
     const [isEdit, setIsEdit] = useState(false);
     const handleClick = () => {
@@ -14,9 +13,9 @@ const Player = ({initialName}) => {
     }
   return (
     <div className="player">
-        {!isEdit && <span>{name}</span>}
+        {!isEdit && <span onClick={handleClick} className='save-edit'>{name}</span>}
         {isEdit && <input type="text" value={name} onChange={handleChange}/>}
-        <span onClick={handleClick}>{isEdit? 'Save' : 'Edit'}</span>
+        <span className='save-edit' onClick={handleClick}>{isEdit? 'Save' : 'Edit'}</span>
       </div>
   )
 }
