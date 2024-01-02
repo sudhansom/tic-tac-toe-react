f81b458916571cc4584c16e550388596f0290821
bhuwan <bkspoudel@gmail.com>
2024-01-02 20:11:06 +0100

    feat: fix the headet in index.html file

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
 
-const Player = ({name, symbol}) => {
+const Player = ({initialName, symbol}) => {
     const [ isEditable, setIsEditable ] = useState(false);
+    const [name, setName] = useState(initialName);
     function handleEditClick(){
         setIsEditable(prev => !prev);
+    }
+    function handleChange(e){
+        setName(e.target.value);
     }
   return (
     <li>
         <span className="player">
-            { isEditable && <input type='text' required/>}
+            { isEditable && <input type='text' value={name} required onChange={handleChange}/>}
             {!isEditable && <span className="player-name">{name}</span>}
             <span className='player-symbol'>{symbol}</span>
         </span>
