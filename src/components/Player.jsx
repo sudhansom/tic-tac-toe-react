e47775c29cc11bfd1ea75601f086833317118bf0
bhuwan <bkspoudel@gmail.com>
2024-01-02 19:53:12 +0100

    make the player component interactive

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Player = ({name, symbol}) => {
+    const [ isEditable, setIsEditable ] = useState(false);
+    function handleEditClick(){
+        setIsEditable(prev => !prev);
+    }
   return (
     <li>
-          <span className="player">
-            <span className="player-name">{name}</span>
+        <span className="player">
+            { isEditable && <input type='text' required/>}
+            {!isEditable && <span className="player-name">{name}</span>}
             <span className='player-symbol'>{symbol}</span>
-          </span>
-          <button>Edit</button>
-        </li>
+        </span>
+        <button onClick={handleEditClick}>{isEditable? 'Save' : 'Edit'}</button>
+    </li>
   )
 }
 
