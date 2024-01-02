281801aa526acc6212f36c4370cf494ca8683eb2
bhuwan <bkspoudel@gmail.com>
2024-01-02 22:21:44 +0100

    feat: set symbol on click

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const initialGameBoard = [
     [null, null, null],
@@ -6,12 +6,24 @@ const initialGameBoard = [
     [null, null, null],
 ]
 
+initialGameBoard[0][1] = 'O';
+
 const GameBoard = () => {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+    const handleClick = (i, j, s) => {
+
+        setGameBoard(prev => {
+            const temp = [...prev].map(innerArray => [...innerArray]);
+            temp[i][j] = 'X';
+            return temp;
+        })
+        
+    }
   return (
    <ol id="game-board">
-    {initialGameBoard.map((row, i)=> <li key={i} >
+    {gameBoard.map((row, i)=> <li key={i} >
         <ol>
-            {row.map((symbol, symbolIndex) => <li key={symbolIndex}><button>{symbol}</button></li> )}
+            {row.map((symbol, j) => <li key={j}><button onClick={()=>handleClick(i, j, symbol)}>{symbol}</button></li> )}
         </ol>
     </li>)}
    </ol>
