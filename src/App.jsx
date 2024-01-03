9e419605c143af841e89cd94d4aa14a71e4dfb0c
bhuwan <bkspoudel@gmail.com>
2024-01-03 12:31:41 +0100

    fix game winning logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Player from './components/Player'
 import GameBoard from './components/GameBoard'
 import Log from './components/Log'
 import './App.css'
+import { WINNING_COMBINATIONS } from './winning-combinatin'
 
 const initialGameBoard = [
   [null, null, null],
@@ -32,6 +33,17 @@ function App() {
         const { row, col} = square;
         gameBoard[row][col] = player;
     }
+    let winner;
+  
+  for (const combination of WINNING_COMBINATIONS){
+    const first = gameBoard[combination[0].row][combination[0].col];
+    const second = gameBoard[combination[1].row][combination[0].col];
+    const third = gameBoard[combination[2].row][combination[0].col];
+
+    if(first && first === second && first === third) {
+      winner = first;
+    }
+  }
 
   const handleSelection = (i, j, s) => {
     // already disabled the button if the symbol exists. choose one way.
@@ -52,6 +64,7 @@ function App() {
         <Player initialName="Player 1" symbol="X" isActive={currentPlayer==='X'} />
         <Player initialName="Player 2" symbol="O" isActive={currentPlayer==='O'}/>
       </ol>
+      {winner && <p>You won {winner === 'X'? 'Player 1' : 'Player 2'} !!!</p>}
       <GameBoard onSelection={handleSelection} board={gameBoard} />
     </div>
     <Log updatedTurns={gameTurns} />
